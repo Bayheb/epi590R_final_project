06d496afa17a841c37cc3188842f5876ae1a9683
Bailey Hebermehl <bheberm@emory.edu>
2026-08-18 16:32:25 -0400

    added histogram

--- code/03_regression_table.R
+++ code/03_regression_table.R
@@ -1,10 +1,18 @@
+#fit mlr model
 mlr_regression_model <- lm(TimeToRecurrence ~ Age + PVol + TVol_label,
 									 data = data
 )
 
+#mlr table
 tbl_regression(
 	mlr_regression_model,
 	label = list(
 		PVol ~ "Prostate Volume",
 		TVol_label ~ "Tumor Volume"
 	))
+
+#histogram
+hist(data$TimeToRecurrence, main = "Histogram #1",
+														xlab = "Time to Recurrence (Months)")
+
+
